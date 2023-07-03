b8ce5d920a6e08452694c41a494c4d5ebed35f96
superantieu <superantieu@gmail.com>
2023-07-03 14:47:06 +0700

    add checkbox in Them, CapNhat

--- Super/Areas/Admin/Controllers/HangController.cs
+++ Super/Areas/Admin/Controllers/HangController.cs
@@ -21,8 +21,7 @@ namespace Super.Areas.Admin.Controllers
         [Route("index")]
         public IActionResult Index(int page = 1, int pageSize = 10)
         {
-            //var supe = new Sup();
-            //var model = supe.ListAllPaging(page, pageSize);
+          
             var model = _context.Hangs.OrderByDescending(x => x.MaHang).ToPagedList(page, pageSize);
             //var item = _context.Hangs.ToList();
             return View(model);
@@ -52,130 +51,71 @@ namespace Super.Areas.Admin.Controllers
             // Update
         }
        
-        [HttpGet]
-        public IActionResult Them()
-        {
-            return View();
-        }
-        [HttpPost]
         
-        public IActionResult Them(Hang model, IFormFile hinhanh)
+        [Route("them")]
+        public IActionResult Them(int? mahang, string? tenhang, string? dongiaban, string? manhanhieu, string? mota, string? chungloai, string? khuyenmai, DateTime? ngaynhap, string? imageurl, IFormFile hinhanh, bool? trangthai)
         {
-           
-                try
-                {
-                    var hang = new Hang
-
-                    {
-                     //   hang.TenHang = tenhang;
-                    //        hang.DonGiaHang = dongiaban;
-                    //        hang.MaNhanHieu = manhanhieu;
-                    //        hang.MoTa = mota;
-                    //        hang.Src = chungloai;
-                    //        hang.Url = khuyenmai;
-                    //        hang.NgayNhap = ngaynhap;
-                    //        hang.IsActive = trangthai;
-                    TenHang = model.TenHang,
-                        DonGiaHang = model.DonGiaHang,
-                        MaNhanHieu = model.MaNhanHieu,
-                        MoTa = model.MoTa,
-                        Src = model.Src,
-                        Url = model.Url,
-                        NgayNhap = model.NgayNhap,
-                        IsActive = model.IsActive,
-
-                    };
-
-                    var entry = _context.Entry(hang);
-                    entry.State = EntityState.Added;
-                    entry.Property("Filter").IsModified = false;
-                    _context.SaveChanges();
-
-                    // kiểm tra xe file ảnh có bị nul hay là không
-                    if (hinhanh != null && hinhanh.Length > 0)
-                    {
-
-                        // tạo đường dẫn dến thư mục lưu ảnh
-                        var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "asset/image", hinhanh.FileName);
-
-                        // tạo luồng để lưu ảnh vào đường dẫn
-                        using (var stream = new FileStream(imagePath, FileMode.Create))
-                        {
-                            hinhanh.CopyTo(stream);
-                        }
-                        // lưu đường dẫn vào database
-                        hang.HinhAnh = "/asset/image/" + hinhanh.FileName;
-                        _context.SaveChanges();
-                    }
-
-
-
-
-                    return RedirectToAction("Index", "Hang", new { area = "Admin" });
-                }
-                catch (Exception ex)
-                {
-
-                }
+            var home = _context.Balances.Where(c => c.IsActive == true).ToList();
+            if (!String.IsNullOrEmpty(tenhang))
+            {
+               
             
-            return View(model);
+                Hang hang = new Hang();
+
+                hang.TenHang = tenhang;
+                hang.DonGiaHang = dongiaban;
+                hang.MaNhanHieu = manhanhieu;
+                hang.MoTa = mota;
+                hang.Src = chungloai;
+                hang.Url = khuyenmai;
+                hang.NgayNhap = ngaynhap;
+                hang.IsActive = trangthai;
+                
+
+               
+                _context.Add(hang);
+                _context.SaveChanges();
+                if (hinhanh != null && hinhanh.Length > 0)
+                {
+
+                    // tạo đường dẫn dến thư mục lưu ảnh
+                    var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "asset/image", hinhanh.FileName);
+
+                    // tạo luồng để lưu ảnh vào đường dẫn
+                    using (var stream = new FileStream(imagePath, FileMode.Create))
+                    {
+                        hinhanh.CopyTo(stream);
+                    }
+                    // lưu đường dẫn vào database
+                    hang.HinhAnh = "/asset/image/" + hinhanh.FileName;
+                    _context.SaveChanges();
+                }
+                return RedirectToAction("Index", "Hang", new { area = "Admin" });
+                //return View(home);
+            }
+            return View(home);
+
         }
-        //[Route("them")]
-        //public IActionResult Them(int? mahang, string? tenhang, string? dongiaban, string? manhanhieu,string? mota, string? chungloai,string? khuyenmai,DateTime? ngaynhap, string? imageurl , IFormFile hinhanh, bool? trangthai)
-        //{
-        //    if (!String.IsNullOrEmpty(tenhang))
-        //    {
-        //        Hang hang = new Hang();
-
-        //        hang.TenHang = tenhang;
-        //        hang.DonGiaHang = dongiaban;
-        //        hang.MaNhanHieu = manhanhieu;
-        //        hang.MoTa = mota;
-        //        hang.Src = chungloai;
-        //        hang.Url = khuyenmai;
-        //        hang.NgayNhap = ngaynhap;
-        //        hang.IsActive = trangthai;
-
-
-        //        //hang.Filter = tenhang.ToLower() + manhanhieu.ToLower() + A.ghg(tenhang.ToLower());
-        //        _context.Add(hang);
-        //        _context.SaveChanges();
-        //        if (hinhanh != null && hinhanh.Length > 0)
-        //        {
-
-        //            // tạo đường dẫn dến thư mục lưu ảnh
-        //            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "asset/image", hinhanh.FileName);
-
-        //            // tạo luồng để lưu ảnh vào đường dẫn
-        //            using (var stream = new FileStream(imagePath, FileMode.Create))
-        //            {
-        //                hinhanh.CopyTo(stream);
-        //            }
-        //            // lưu đường dẫn vào database
-        //            hang.HinhAnh = "/asset/image/" + hinhanh.FileName;
-        //            _context.SaveChanges();
-        //        }
-        //        return RedirectToAction("Index", "Hang", new { area = "Admin" });
-        //    }
-        //    return View();
-
-        //}
         [Route("cap-nhat")]
         public IActionResult CapNhat(int? mahang, string? tenhang, string? dongiaban, string? manhanhieu, string? mota, string? chungloai, string? khuyenmai, DateTime? ngayban, IFormFile hinhanh, bool? isUpdate = true)
         {
-
+            HomeData hang = new HomeData();
             var itemToUpdate = _context.Hangs.FirstOrDefault(x => x.MaHang == mahang);
+            var kmItem = _context.Balances.Where(c => c.IsActive == true).ToList();
+            var item = _context.Hangs.Where(c => c.MaHang == mahang).ToList();
+            hang.KMDB = kmItem;
+            hang.DSH = item;
 
             if (!(bool)isUpdate)
             {
-                return View(itemToUpdate);
+                return View(hang);
             }
             else
             {
                 itemToUpdate.TenHang = tenhang;
                 itemToUpdate.DonGiaHang = dongiaban;
                 itemToUpdate.MaNhanHieu = manhanhieu;
-                
+
                 itemToUpdate.MoTa = mota;
                 itemToUpdate.Src = chungloai;
                 itemToUpdate.Url = khuyenmai;
@@ -199,7 +139,48 @@ namespace Super.Areas.Admin.Controllers
                 }
                 return RedirectToAction("Index", "Hang", new { area = "Admin" });
             }
-        }
+            
+        }  
+        //[Route("cap-nhat")]
+        //public IActionResult CapNhat(int? mahang, string? tenhang, string? dongiaban, string? manhanhieu, string? mota, string? chungloai, string? khuyenmai, DateTime? ngayban, IFormFile hinhanh, bool? isUpdate = true)
+        //{
+
+        //    var itemToUpdate = _context.Hangs.FirstOrDefault(x => x.MaHang == mahang);
+
+        //    if (!(bool)isUpdate)
+        //    {
+        //        return View(itemToUpdate);
+        //    }
+        //    else
+        //    {
+        //        itemToUpdate.TenHang = tenhang;
+        //        itemToUpdate.DonGiaHang = dongiaban;
+        //        itemToUpdate.MaNhanHieu = manhanhieu;
+
+        //        itemToUpdate.MoTa = mota;
+        //        itemToUpdate.Src = chungloai;
+        //        itemToUpdate.Url = khuyenmai;
+        //        itemToUpdate.NgayNhap = ngayban;
+        //        _context.Update(itemToUpdate);
+        //        _context.SaveChanges();
+        //        if (hinhanh != null && hinhanh.Length > 0)
+        //        {
+
+        //            // tạo đường dẫn dến thư mục lưu ảnh
+        //            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "asset/image", hinhanh.FileName);
+
+        //            // tạo luồng để lưu ảnh vào đường dẫn
+        //            using (var stream = new FileStream(imagePath, FileMode.Create))
+        //            {
+        //                hinhanh.CopyTo(stream);
+        //            }
+        //            // lưu đường dẫn vào database
+        //            itemToUpdate.HinhAnh = "/asset/image/" + hinhanh.FileName;
+        //            _context.SaveChanges();
+        //        }
+        //        return RedirectToAction("Index", "Hang", new { area = "Admin" });
+        //    }
+        //}
         [Route("search")]
         public IActionResult Search(string searchData)
         {
@@ -217,6 +198,11 @@ namespace Super.Areas.Admin.Controllers
                 return View();
             }
 
+        }
+        public IActionResult _partialKM()
+        {
+
+            return PartialView();
         }
 
     }
